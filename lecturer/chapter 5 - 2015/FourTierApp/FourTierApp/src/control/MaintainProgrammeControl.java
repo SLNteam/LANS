aafb5d92b37f0057f9b70adc37010cdc4c1c2ddd
Student <Student@cslab08>
2016-11-18 15:07:43 +0800

    No we don't type commit message

---
+++ lecturer/chapter 5 - 2015/FourTierApp/FourTierApp/src/control/MaintainProgrammeControl.java
@@ -0,0 +1,33 @@
+package control;
+
+import da.ProgrammeDA;
+import domain.Programme;
+
+public class MaintainProgrammeControl {
+
+    private ProgrammeDA progDA;
+
+    public MaintainProgrammeControl() {
+        progDA = new ProgrammeDA();
+    }
+
+    public Programme selectRecord(String code) {
+        return progDA.getRecord(code);
+    }
+    
+    public void addRecord(Programme p){
+        progDA.addRecord(p);
+        
+    }
+    
+    public void updateRecord(Programme p){
+        progDA.UpdateRecord(p);
+        
+    }
+    public void deleteRecord(String code){
+        progDA.DeleteRecord(code);
+        
+    }
+    
+
+}
